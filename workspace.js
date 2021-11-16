5c94daffb795fc75e72e1f84e59ce91f62e34e3f
Lefteris Eleftheriades <lefteris.eleftheriades@gmail.com>
2021-11-16 18:07:23 +0200

    connected toolbar button

--- workspace.js
+++ workspace.js
@@ -184,3 +184,35 @@ function addIOElement () {
   const workspace = document.getElementById("componentLayer");
   workspace.appendChild(group);
 }
+
+function addProcessElement(){
+  const group = document.createElementNS("http://www.w3.org/2000/svg","g");
+  group.classList.add("draggable-group");
+  
+  const outline = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+  outline.classList.add("outline");
+  outline.setAttribute("x", "0");
+  outline.setAttribute("y", "0");
+  outline.setAttribute("width", "155");
+  outline.setAttribute("height", "40");
+  outline.setAttribute("fill", "white");
+  outline.setAttribute("stroke", "black");
+  
+  const label = document.createElementNS("http://www.w3.org/2000/svg","text");
+  label.classList.add("text", "unselectable");
+  label.setAttribute("x", "10");
+  label.setAttribute("y", "23");
+  label.setAttribute("font-family", "Verdana");
+  label.setAttribute("font-size", "10");
+  label.setAttribute("fill", "black");
+  label.setAttribute("stroke", "none");
+  
+  const content = document.createTextNode('out("Your name is", name)');
+  label.appendChild(content);
+  
+  group.appendChild(outline);
+  group.appendChild(label);
+  
+  const workspace = document.getElementById("componentLayer");
+  workspace.appendChild(group);
+}
